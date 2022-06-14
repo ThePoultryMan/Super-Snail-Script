bbfac0da48fddb14af239df6c51981fe90cb0b5c
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-13 21:22:40 -0400

    Merge branch 'dev/logger' into main

--- src/main/java/io/github/thepoultryman/snailscript/SuperSnailScript.java
+++ src/main/java/io/github/thepoultryman/snailscript/SuperSnailScript.java
@@ -1,9 +1,21 @@
 package io.github.thepoultryman.snailscript;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 
 public class SuperSnailScript {
+    public static final Logger LOGGER = LoggerFactory.getLogger("snailscript");
+
     public static void main(String[] args) {
-        File file = new File(args[0]);
+        if (args.length >= 1) {
+            File file = new File(args[0]);
+            if (!file.exists()) {
+                LOGGER.error("The specified file does not exist! Please check to make sure that the path that you entered is correct.");
+            }
+        } else {
+            LOGGER.error("No arguments were provided, and therefore no file could be read.");
+        }
     }
 }
