42df6ef3988d8909efe980dba50ab8e5f9c13f89
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-06-13 21:51:48 -0400

    Initial file reading

--- src/main/java/io/github/thepoultryman/snailscript/reader/BasicReader.java
+++ src/main/java/io/github/thepoultryman/snailscript/reader/BasicReader.java
@@ -1,11 +1,29 @@
 package io.github.thepoultryman.snailscript.reader;
 
+import io.github.thepoultryman.snailscript.SuperSnailScript;
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class BasicReader {
-    private final File file;
+    private List<String> lines = null;
 
     public BasicReader(File file) {
-        this.file = file;
+        try {
+            this.lines = FileUtils.readLines(file, StandardCharsets.UTF_8);
+            SuperSnailScript.LOGGER.info("All of the lines were successfully read, and your script will now be run.");
+            this.runScript();
+        } catch (IOException e) {
+            SuperSnailScript.LOGGER.error("There was an issue reading the lines of your script. This is the end of any attempt to run the script.");
+        }
+    }
+
+    private void runScript() {
+        for (String line : lines) {
+            SuperSnailScript.LOGGER.info(line);
+        }
     }
 }
